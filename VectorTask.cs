da0aa613237bc8f665049f7e6f379c502e32df5a
Sergey Chebotar <s.chebotar@gmail.com>
2020-03-03 21:06:57 +0300

    Add project files.

---
+++ VectorTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+    }
+
+    public class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(vector.X*vector.X + vector.Y*vector.Y);
+        }
+
+        public static Vector Add(Vector a, Vector b)
+        {
+            var result = new Vector();
+            result.X = a.X + b.X;
+            result.Y = a.Y + b.Y;
+
+            return result;
+        }
+    }
+}
